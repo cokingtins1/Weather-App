ea6ac50a2d01a366a5b2eaceb99661de501c0b42
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-08-05 18:22:51 -0400

    layout done

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-// let myPromise = new Promise((resolve, reject) => {
-// 	let num = Math.floor(Math.random() * 10) + 1;
-// 	console.log(num);
+// // let myPromise = new Promise((resolve, reject) => {
+// // 	let num = Math.floor(Math.random() * 10) + 1;
+// // 	console.log(num);
+// // 	if (num < 5) {
+// // 		resolve("you have guessed correctly");
+// // 	} else {
+// // 		reject("better luck next time");
+// // 	}
+// // });
+
+// // // resolve call back function
+// // handleSuccess = (goodNews) => {
+// // 	console.log(goodNews);
+// // };
+
+// // // reject call back function
+// // handleFailure = (badNews) => {
+// // 	console.log(badNews);
+// // };
+
+// // Invoke promise. pass in resolve and reject values
+// // myPromise.then(handleSuccess, handleFailure);
+
+// async function randomResponse () {
+//     let message = ''
+//     let num = Math.floor(Math.random() * 10) + 1;
+// 	console.log(num)
 // 	if (num < 5) {
-// 		resolve("you have guessed correctly");
+// 		message = await ('you guessed correctly')
+//         console.log(message)
 // 	} else {
-// 		reject("better luck next time");
+// 		message = await ('better luck next time');
+//         console.log(message)
 // 	}
-// });
+// }
 
-// // resolve call back function
-// handleSuccess = (goodNews) => {
-// 	console.log(goodNews);
-// };
+// randomResponse()
 
-// // reject call back function
-// handleFailure = (badNews) => {
-// 	console.log(badNews);
-// };
+// const arrayOfFruits = ['bananna', 'apples', 'oranges']
 
-// Invoke promise. pass in resolve and reject values
-// myPromise.then(handleSuccess, handleFailure);
+// arrayOfFruits.forEach(async fruit => {
+//     let message = "would you like " + fruit + "'?"
+//     console.log(message)
+// })
 
-async function randomResponse () {
-    let message = ''
-    let num = Math.floor(Math.random() * 10) + 1;
-	console.log(num)
-	if (num < 5) {
-		message = await ('you guessed correctly')
-        console.log(message)
-	} else {
-		message = await ('better luck next time');
-        console.log(message)
-	}
-}
+// const img = document.querySelector('img')
 
-randomResponse()
+// async function getCats() {
+//     const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=vsrc6JCfbh3JGdpBGonu9hxrZpS85bMc&s=cats', {mode: 'cors'})
+//     const catData = await response.json()
+//     img.src = catData.data.images.original.url;
+// }
 
-const arrayOfFruits = ['bananna', 'apples', 'oranges']
-
-arrayOfFruits.forEach(async fruit => {
-    let message = "would you like " + fruit + "'?"
-    console.log(message)
-})
-
-const img = document.querySelector('img')
-
-async function getCats() {
-    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=vsrc6JCfbh3JGdpBGonu9hxrZpS85bMc&s=cats', {mode: 'cors'})
-    const catData = await response.json()
-    img.src = catData.data.images.original.url;
-}
-
-getCats()
+// getCats()
